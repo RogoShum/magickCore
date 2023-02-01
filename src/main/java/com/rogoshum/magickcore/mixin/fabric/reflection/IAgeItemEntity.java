5c542b6ebe07405197279f663c3a1814ea29c0a7
RogoShum <1269314290@qq.com>
2023-02-01 09:37:30 +0800

    migrating...60.5%

--- src/main/java/com/rogoshum/magickcore/mixin/fabric/reflection/IAgeItemEntity.java
+++ src/main/java/com/rogoshum/magickcore/mixin/fabric/reflection/IAgeItemEntity.java
@@ -1,7 +1,15 @@
 package com.rogoshum.magickcore.mixin.fabric.reflection;
 
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ItemEntity.class)
 public interface IAgeItemEntity {
+    @Accessor("pickupDelay")
     int getPickupDelay();
+    @Accessor("age")
     int getAge();
+    @Accessor("age")
     void setAge(int age);
 }
